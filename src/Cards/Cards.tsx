f613fc43460608f3e3b55dc084769efd747d609d
Salil Saurav <salilsaurav.work@gmail.com>
2023-10-21 22:10:16 +0530

    cards added

--- src/Cards/Cards.tsx
+++ src/Cards/Cards.tsx
@@ -1,5 +1,9 @@
 import "./Cards.css";
 import Tilt from "react-parallax-tilt";
+import campImage from "../assets/images/campsite.png";
+import cardImageTwo from "../assets/images/PerfectDry.png";
+import cardImageThree from "../assets/images/thecrew.png";
+import cardFourImage from "../assets/images/livesearch.png";
 
 const Cards = () => {
   return (
@@ -7,20 +11,55 @@ const Cards = () => {
       <div className="card-group-one">
         <Tilt tiltEnable={true}>
           <div className="card">
-            <h2>Projects</h2>
-            <p>https://campsite-depn.onrender.com</p>
+            <div className="cardOne">
+              <a href="https://campsite-mym1.onrender.com" target="_blank">
+                <img className="cardImage" src={campImage} alt="campImage" />
+              </a>
+            </div>
           </div>
         </Tilt>
         <Tilt tiltEnable={true}>
-          <div className="card"></div>
+          <div className="card">
+            <div className="cardTwo">
+              <a href="https://rad-strudel-251366.netlify.app" target="_blank">
+                <img
+                  className="cardImage"
+                  src={cardImageTwo}
+                  alt="Card Two Image"
+                />
+              </a>
+            </div>
+          </div>
         </Tilt>
       </div>
       <div className="card-group-two">
         <Tilt tiltEnable={true}>
-          <div className="card">card 3</div>
+          <div className="card">
+            {" "}
+            <div className="cardThree">
+              <a href="https://mellow-dodol-8c8c4d.netlify.app" target="_blank">
+                <img
+                  className="cardImage"
+                  src={cardImageThree}
+                  alt="Card Three Image"
+                />
+              </a>
+            </div>
+          </div>
         </Tilt>
         <Tilt tiltEnable={true}>
-          <div className="card">card 4</div>
+          <div className="card">
+            {" "}
+            <div className="cardFour">
+              <a href="https://deluxe-froyo-e9d9c8.netlify.app" target="_blank">
+                <img
+                  className="cardImage"
+                  src={cardFourImage}
+                  alt="Card Four Image"
+                />
+              </a>
+            </div>
+          </div>
         </Tilt>
       </div>
     </>
